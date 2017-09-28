5c83ed2463b0cb0d6472577f7b10d80d71e8b466
Will Olson <will.olson11@gmail.com>
2017-09-28 13:59:45 -0700

    feat: Update redux on submit

--- utils/api.js
+++ utils/api.js
@@ -1,18 +1,18 @@
-import { AsynStorage } from 'react-native';
+import { AsyncStorage } from 'react-native';
 import { CALENDAR_STORAGE_KEY } from './_calendar';
 
 export function submitEntry ({ entry, key }) {
-  return AsynStorage.mergeItem(CALENDAR_STORAGE_KEY, JSON.stringify({
+  return AsyncStorage.mergeItem(CALENDAR_STORAGE_KEY, JSON.stringify({
     [key]: entry,
   }));
 }
 
 export function removeEntry (key) {
-  return AsynStorage.getItem(CALENDAR_STORAGE_KEY)
+  return AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
     .then(results => {
       const data = JSON.parse(results);
       data[key] = undefined;
       delete data[key];
-      AsynStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data));
+      AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data));
     });
 }
