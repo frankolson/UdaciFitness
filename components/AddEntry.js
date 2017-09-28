ba0834e79b37b999559234e76326cf092319963d
Will Olson <will.olson11@gmail.com>
2017-09-27 18:40:07 -0700

    feat: Add reset page

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
 import { getMetricMetaInfo, timeToString } from '../utils/helpers';
+import { Ionicons } from '@expo/vector-icons';
 
 import DateHeader from './DateHeader';
 import UdaciSlider from './UdaciSlider';
 import UdaciSteppers from './UdaciSteppers';
+import TextButton from './TextButton';
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -76,8 +78,35 @@ export default class AddEntry extends Component {
     // Clear local notification
   }
 
+  reset = () => {
+    const key = timeToString();
+
+    // Update Redux
+
+    // Save to 'DB'
+
+    // Clear local notification
+  }
+
   render() {
     const metaInfo = getMetricMetaInfo();
+
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons
+            name="ios-happy-outline"
+            size={100}
+          />
+
+          <Text>You already logged your information for today.</Text>
+
+          <TextButton onPress={this.reset}>
+            Reset
+          </TextButton>
+        </View>
+      )
+    }
 
     return (
       <View>
