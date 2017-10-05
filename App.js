3ebd916e242d354d752125e8bd79bcb62e43530b
Will Olson <will.olson11@gmail.com>
2017-10-04 18:51:57 -0700

    feat: Build initial structure of History component

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import AddEntry from './components/AddEntry';
+import History from './components/History';
 import reducer from './reducers';
 
 export default class App extends React.Component {
@@ -11,7 +12,7 @@ export default class App extends React.Component {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
-          <AddEntry />
+          <History />
         </View>
       </Provider>
     );
