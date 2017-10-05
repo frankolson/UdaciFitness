7c5ffacfb3eb5193d78c6d318591cc6b3ed46170
Will Olson <will.olson11@gmail.com>
2017-10-05 08:31:43 -0700

    feat: Add UdaciFitnessCalendar

--- components/History.js
+++ components/History.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
+import UdaciFitnessCalendar from 'udacifitness-calendar';
 
 import { receiveEntries, addEntry } from '../actions';
 import { timeToString, getDailyReminderValue } from '../utils/helpers';
@@ -20,11 +21,33 @@ class History extends Component {
         }
       });
   }
-  render() {
+
+  renderItem = ({ today, ...metrics }, formattedDate, key) => (
+    <View>
+      {today
+        ? <Text>{JSON.stringify(today)}</Text>
+        : <Text>{JSON.stringify(metrics)}</Text>
+      }
+    </View>
+  )
+
+  renderEmptyDate(formattedDate) {
     return (
       <View>
-        <Text>{JSON.stringify(this.props)}</Text>
+        <Text>No data for this day</Text>
       </View>
+    )
+  }
+
+  render() {
+    const { entries } = this.props;
+
+    return (
+      <UdaciFitnessCalendar
+        items={entries}
+        renderItem={this.renderItem}
+        renderEmptyDate={this.renderEmptyDate}
+      />
     )
   }
 };
